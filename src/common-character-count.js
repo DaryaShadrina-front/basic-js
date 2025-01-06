ead172b8120168ad2f34bbead24da14975cb2e17
Darya Shadrina <daryashadrina.frontend@gmail.com>
2025-01-06 22:50:05 +0300

    feat: added solutions task CommonCharacterCount and deleteDigit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if (s1.length === 0 || s2.length === 0) return 0;
+
+  let splitS2 = s2.split('');
+  let count = 0;
+
+  for (let i = 0; i < s1.length; i += 1) {
+    let currentIndex = splitS2.indexOf(s1[i]);
+    if (currentIndex !== -1) {
+      count += 1;
+      splitS2.splice(currentIndex, 1);
+    }
+  }
+  return count;
 }
 
 module.exports = {
